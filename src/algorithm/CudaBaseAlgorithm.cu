2e14b939d9e1f88e0dd95bc541906b595f55b560
Sigurd Storve <sigurd.storve@ntnu.no>
2015-09-17 20:27:23 +0200

    Changed extensions to .cu and .cuh and updated client code

--- src/algorithm/CudaBaseAlgorithm.cu
+++ src/algorithm/CudaBaseAlgorithm.cu
@@ -28,7 +28,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 #include <stdexcept>
 #include <iostream>
-#include "CudaBaseAlgorithm.hpp"
+#include "CudaBaseAlgorithm.cuh"
 
 namespace bcsim {
 CudaBaseAlgorithm::CudaBaseAlgorithm()
