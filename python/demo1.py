fd6c21039756400c5522e484d415f7d36caf23b5
Sigurd Storve <sigurd.storve@ntnu.no>
2016-01-04 19:34:02 +0100

    Updated Python scripts to reflect changes to the simulator.

--- python/demo1.py
+++ python/demo1.py
@@ -2,14 +2,14 @@
 # Linear scan with three scatterers.
 # Using a Gaussian analytic beam profile.
 import sys
-sys.path.append('.')
+sys.path.append(".")
 from pyrfsim import RfSimulator
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 from scipy.signal import gausspulse
 import numpy as np
 
-# Create and configure tracker
+# Create and configure simulator
 sim = RfSimulator("fixed")
 sim.set_parameter("verbose", "1")
 sim.set_print_debug(True)
@@ -17,10 +17,11 @@ sim.set_print_debug(True)
 # Set general simulation parameters
 sim.set_parameter("sound_speed", "1540.0")
 sim.set_parameter("num_cpu_cores", "all")
+sim.set_parameter("radial_decimation", "20")
 
 # Set scatterers
 num_scatterers = 16
-scatterers_data = np.zeros((num_scatterers, 4), dtype='float32')
+scatterers_data = np.zeros((num_scatterers, 4), dtype="float32")
 scatterers_data[:,2] = np.linspace(0.01, 0.16, num_scatterers)
 scatterers_data[:,3] = np.ones((num_scatterers,))
 sim.set_fixed_scatterers(scatterers_data)
@@ -33,18 +34,18 @@ tc = 1.0/fc
 t_vector = np.arange(-16*tc, 16*tc, ts)
 bw = 0.5
 plt.figure(1)
-samples = np.array(gausspulse(t_vector, bw=bw, fc=fc), dtype='float32')
+samples = np.array(gausspulse(t_vector, bw=bw, fc=fc), dtype="float32")
 center_index = int(len(t_vector)/2) 
 plt.plot(t_vector, samples);
-plt.title('Excitation signal')
-plt.xlabel('Time [s]')
-plt.ylabel('Exitation')
+plt.title("Excitation signal")
+plt.xlabel("Time [s]")
+plt.ylabel("Exitation")
 plt.show()
-sim.set_excitation(samples, center_index, fs)
+sim.set_excitation(samples, center_index, fs, fc)
 
 # Define a scan sequence
 num_lines = 12
-origins = np.zeros((num_lines, 3), dtype='float32')
+origins = np.zeros((num_lines, 3), dtype="float32")
 origins[:,0] = np.linspace(-0.04, 0.04, num_lines)
 
 
@@ -52,10 +53,10 @@ x_axis = np.array([1.0, 0.0, 0.0])
 y_axis = np.array([0.0, 1.0, 0.0])
 z_axis = np.array([0.0, 0.0, 1.0])
 num_lines = origins.shape[0]
-directions = np.array(np.tile(z_axis, (num_lines, 1)), dtype='float32')
+directions = np.array(np.tile(z_axis, (num_lines, 1)), dtype="float32")
 length = 0.20
-lateral_dirs = np.array(np.tile(x_axis, (num_lines, 1)), dtype='float32')
-timestamps = np.zeros((num_lines,), dtype='float32')
+lateral_dirs = np.array(np.tile(x_axis, (num_lines, 1)), dtype="float32")
+timestamps = np.zeros((num_lines,), dtype="float32")
 sim.set_scan_sequence(origins, directions, length, lateral_dirs, timestamps)
 
 
@@ -74,6 +75,5 @@ rf_lines = np.real(abs(rf_lines))
 plt.figure(2);
 dyn_range = 50;
 gain = 30;
-#img = rf_to_image(rfLines', gain, dyn_range);
-plt.imshow(rf_lines, interpolation='nearest', cmap=cm.Greys_r, aspect='auto');
+plt.imshow(rf_lines, interpolation="nearest", cmap=cm.Greys_r, aspect="auto");
 plt.show()
--- python/linear_scan_phantom.py
+++ python/linear_scan_phantom.py
@@ -14,7 +14,7 @@ description="""
     runs.
 """
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = argparse.ArgumentParser(description=description)
     parser.add_argument("h5_file", help="Hdf5 file with scatterers")
     parser.add_argument("--x0", help="Left scan width", type=float, default=-1e-2)
@@ -44,6 +44,7 @@ print "The number of scatterers is %d" % scatterers_data.shape[0]
 
 # configure simulation parameters
 sim.set_parameter("sound_speed", "1540.0")
+sim.set_parameter("radial_decimation", "30")
 
 # configure the RF excitation
 fs = 100e6
@@ -52,19 +53,19 @@ fc = 2.5e6
 tc = 1.0/fc
 t_vector = np.arange(-16*tc, 16*tc, ts)
 bw = 0.2
-samples = np.array(gausspulse(t_vector, bw=bw, fc=fc), dtype='float32')
+samples = np.array(gausspulse(t_vector, bw=bw, fc=fc), dtype="float32")
 center_index = int(len(t_vector)/2) 
-sim.set_excitation(samples, center_index, fs)
+sim.set_excitation(samples, center_index, fs, fc)
 
 # define the scan sequence
-origins = np.zeros((args.num_lines, 3), dtype='float32')
+origins = np.zeros((args.num_lines, 3), dtype="float32")
 origins[:,0] = np.linspace(args.x0, args.x1, args.num_lines)
 x_axis = np.array([1.0, 0.0, 0.0])
 z_axis = np.array([0.0, 0.0, 1.0])
-directions = np.array(np.tile(z_axis, (args.num_lines, 1)), dtype='float32')
+directions = np.array(np.tile(z_axis, (args.num_lines, 1)), dtype="float32")
 length = 0.12
-lateral_dirs = np.array(np.tile(x_axis, (args.num_lines, 1)), dtype='float32')
-timestamps = np.zeros((args.num_lines,), dtype='float32')
+lateral_dirs = np.array(np.tile(x_axis, (args.num_lines, 1)), dtype="float32")
+timestamps = np.zeros((args.num_lines,), dtype="float32")
 sim.set_scan_sequence(origins, directions, length, lateral_dirs, timestamps)
 
 # configure the beam profile
@@ -81,25 +82,25 @@ rf_lines = np.real(abs(rf_lines))
     
 if args.save_simdata_file != None:
     with h5py.File(args.save_simdata_file, "w") as f:
-        f["sim_data"] = np.array(rf_lines, dtype='float32')
+        f["sim_data"] = np.array(rf_lines, dtype="float32")
     print "Simulation output written to %s" % args.save_simdata_file
     
-print 'Simulation time: %f +- %f s  (N=%d)' % (np.mean(frame_sim_times), np.std(frame_sim_times), args.num_frames)    
+print "Simulation time: %f +- %f s  (N=%d)" % (np.mean(frame_sim_times), np.std(frame_sim_times), args.num_frames)    
 
 if args.save_pdf or args.visualize:
     import matplotlib as mpl
-    mpl.use('Agg')
+    mpl.use("Agg")
     import matplotlib.pyplot as plt
     num_samples, num_lines = rf_lines.shape
     plt.figure(1)
     plt.plot(rf_lines[:, num_lines/2])
     if args.save_pdf: plt.savefig("frame1-out.pdf")
     plt.figure(2)
-    plt.imshow(rf_lines, aspect='auto')
+    plt.imshow(rf_lines, aspect="auto")
     if args.save_pdf: plt.savefig("frame2-out.pdf")
 if args.visualize:
     plt.show()
 if args.save_pdf:
-    print 'Image written to disk.'
+    print "Image written to disk."
     
     
