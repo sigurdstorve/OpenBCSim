9ce22a9cb8602adf8cf1bbf02890a07d00bb3c3d
Sigurd Storve <sigurd.storve@ntnu.no>
2016-01-28 11:34:27 +0100

    Removed unused functionality

--- src/core/algorithm/cuda_helpers.h
+++ src/core/algorithm/cuda_helpers.h
@@ -152,21 +152,8 @@ inline __host__ __device__ float3 operator*(float3 a, float b) {
 
 // Complex datatype
 typedef float2 Complex;
-__device__ __host__ inline Complex ComplexAdd(Complex a, Complex b) {
-    Complex c;
-    c.x = a.x + b.x;
-    c.y = a.y + b.y;
-    return c;
-}
 
-__device__ __host__ inline Complex ComplexScale(Complex a, float s) {
-    Complex c;
-    c.x = s*a.x;
-    c.y = s*a.y;
-    return c;
-}
-
-__device__ __host__ inline Complex ComplexMul(Complex a, Complex b) {
+__device__ __host__ inline Complex ComplexMul(Complex a, Complex b) { // cuCmulf (cuFloatComplex x, cuFloatComplex y)
     Complex c;
     c.x = a.x*b.x - a.y*b.y;
     c.y = a.x*b.y + a.y*b.x;
