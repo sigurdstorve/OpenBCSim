6d67d00f60bb35896229e0220d10719d56b744cc
Sigurd Storve <sigurd.storve@ntnu.no>
2016-02-24 22:12:11 +0100

    Made tissue-with-flow phantom more general.
    It now supports a wide class of flow profiles, i.e. constant or parabolic,
    by adjusting an exponent.

--- phantom_scripts/tissue_with_flow.py
+++ phantom_scripts/tissue_with_flow.py
@@ -4,14 +4,21 @@ import h5py
 import bsplines
 
 description="""
-    Phantom which models tissue with fixed scatterers
-    and simple flow using dynamic spline scatterers.
-    Spline degreee is two.
+    Phantom with both stationary tissue and dynamic flow. The flow
+    scatterers are splines and the velocity profile can be controlled
+    by adjusting the peak velocity (in the center of the vessel) and
+    an exponent, where a value of two gives parabolic, while a high
+    value approximates constant flow.
+    
+    NOTE: Currently, more than the required number of control points
+    are used. In theory, it should be enough with two control points
+    as the motion of each scatterer is in a straight line with 
+    constant velocity. This will be fixed in the future.
 """
 
 def create_fixed_scatterers(args, h5_f):
     common_size = (args.num_tissue_scatterers,)
-    xs = np.random.uniform(low=-0.5*args.x_length, high=0.5*args.x_length, size=common_size)
+    xs = np.random.uniform(low=-0.5*args.tissue_length, high=0.5*args.tissue_length, size=common_size)
     ys = np.random.uniform(low=-0.5*args.box_dim, high=0.5*args.box_dim, size=common_size)
     zs = np.random.uniform(low=0.0, high=args.box_dim, size=common_size)
     ampls = np.random.uniform(low=-1.0, high=1.0, size=common_size)
@@ -31,38 +38,48 @@ def create_spline_scatterers(args, h5_f):
     num_scatterers = args.num_flow_scatterers
     spline_degree = 2
 
-    knot_vector = bsplines.uniform_regular_knot_vector(args.num_cs, spline_degree, t0=0.0, t1=1.001)
+    knot_vector = bsplines.uniform_regular_knot_vector(args.num_cs, spline_degree, t0=args.t0, t1=args.t1+0.001)
     knot_vector = np.array(knot_vector, dtype="float32")
     knot_avgs = bsplines.control_points(spline_degree, knot_vector)
 
-    L = args.x_length
-    # starting positions uniformly distributed in [-1.5L, 0.5L]
-    xs = np.random.uniform(low=-1.5*L, high=0.5*L, size=(num_scatterers))
-    
-    # Assuming t=0...1
-    #   function of time: x(t) = (1-t)*x0 + t*(x0+L) = x0 - t*x0 + t*x0 + t*L
-    #                          = x0 + t*L
+    total_t = args.t1-args.t0
 
+    # create random scatterers in a cylinder centered around the x-axis
     common_size = (num_scatterers,)
     ys = np.random.uniform(low=-args.radius, high=args.radius, size=common_size)
     zs = np.random.uniform(low=-args.radius, high=args.radius, size=common_size)
-    ampls = np.random.uniform(low=-1.0, high=1.0, size=common_size)*args.flow_ampl_factor
-    
-    keep_inds = ys**2 + zs**2 < args.radius**2
-    xs = xs[keep_inds]
+    rs = np.sqrt(ys**2 + zs**2)
+    keep_inds = rs <= args.radius
     ys = ys[keep_inds]
-    zs = zs[keep_inds] + 0.5*args.box_dim
-    ampls = ampls[keep_inds]
+    zs = zs[keep_inds] 
+    rs = rs[keep_inds]
+
+    num_scatterers = len(rs)
+    ampls = np.random.uniform(low=-1.0, high=1.0, size=(num_scatterers,))*args.flow_ampl_factor
+
+    # currenly only support for t0=0
+    assert(abs(args.t0) < 1e-6)
     
-    num_scatterers = len(xs)
+    x_min = -0.5*args.tissue_length - args.peak_velocity*total_t
+    x_max = 0.5*args.tissue_length
+    print "x components in [%f, %f]" % (x_min, x_max)
+    xs = np.random.uniform(low=x_min, high=x_max, size=(num_scatterers,))
+    
+    # velocity along x-axis will be a function of the radius computed above
+    # (1-(r/R)**K), where K controls the shape
+    
     control_points = np.zeros( (num_scatterers, args.num_cs, 3), dtype="float32")
     for cs_i, t_star in enumerate(knot_avgs):
         print "Computing control points for knot average %d of %d" % (cs_i+1, args.num_cs)
 
         # Compute control point position. Amplitude is unchanged.
-        control_points[:, cs_i, 0] = np.array(xs) + t_star*L
+        velocities = args.peak_velocity*(1.0-(rs/args.radius)**args.exponent)
+        print velocities
+        control_points[:, cs_i, 0] = np.array(xs) + t_star*velocities
         control_points[:, cs_i, 1] = np.array(ys)
-        control_points[:, cs_i, 2] = np.array(zs)
+        
+        # shift down along positive z-axis
+        control_points[:, cs_i, 2] = np.array(zs) + 0.5*args.box_dim
 
     # Write results to disk
     with h5py.File(args.h5_file) as f:
@@ -80,12 +97,16 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description=description)
     parser.add_argument("h5_file", help="Output file")
     parser.add_argument("--num_tissue_scatterers", help="Number in initial box", type=int, default=1000000)
-    parser.add_argument("--num_flow_scatterers", help="Number of dynamic flow scatterers", type=int, default=200000)
+    parser.add_argument("--num_flow_scatterers", help="Number of dynamic flow scatterers", type=int, default=1000000)
     parser.add_argument("--box_dim", help="yz dimensions of box [m]", type=float, default=0.03)
     parser.add_argument("--radius", help="Tube radius [m]", type=float, default=0.008)
-    parser.add_argument("--x_length", type=float, default=8e-2)
+    parser.add_argument("--tissue_length", type=float, default=8e-2)
     parser.add_argument("--num_cs", help="Number of control points for each spline", type=int, default=6)
     parser.add_argument("--flow_ampl_factor", help="Flow ampltiude scale factor", type=float, default=0.3)
+    parser.add_argument("--peak_velocity", help="Peak velocity of flow [m/s]", type=float, default=15e-2)
+    parser.add_argument("--t0", help="Start time [s]", type=float, default=0.0)
+    parser.add_argument("--t1", help="End time [s]", type=float, default=1.0)
+    parser.add_argument("--exponent", help="Controls the shape of the velocity profile (2 gives parabolic)", type=float, default=2)
     args = parser.parse_args()
     
     create_phantom(args)
