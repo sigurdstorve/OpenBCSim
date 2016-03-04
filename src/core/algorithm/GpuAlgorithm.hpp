245bfce995892e57da2bae93f4eee6c20635d70e
Sigurd Storve <sigurd.storve@ntnu.no>
2016-03-04 14:06:48 +0100

    Added work-in-progress class for representing device scatterer datasets

--- src/core/algorithm/GpuAlgorithm.hpp
+++ src/core/algorithm/GpuAlgorithm.hpp
@@ -42,6 +42,147 @@ inline float3 to_float3(const bcsim::vector3& v) {
     return make_float3(v.x, v.y, v.z);
 }
 
+// Device memory for a fixed-scatterer dataset.
+class DeviceFixedScatterers {
+public:
+    typedef std::shared_ptr<DeviceFixedScatterers> s_ptr;
+
+    // Allocate space for a new dataset with num_scatterers scatterers.
+    explicit DeviceFixedScatterers(size_t num_scatterers)
+        : m_num_scatterers(num_scatterers)
+    {
+        const auto num_bytes = num_scatterers*sizeof(float);
+        xs = DeviceBufferRAII<float>::u_ptr(new DeviceBufferRAII<float>(num_bytes));
+        ys = DeviceBufferRAII<float>::u_ptr(new DeviceBufferRAII<float>(num_bytes));
+        zs = DeviceBufferRAII<float>::u_ptr(new DeviceBufferRAII<float>(num_bytes));
+        as = DeviceBufferRAII<float>::u_ptr(new DeviceBufferRAII<float>(num_bytes));
+    }
+    
+    size_t get_num_scatterers() const {
+        return m_num_scatterers;
+    }
+
+    float* get_xs_ptr() const {
+        return xs->data();
+    }
+
+    float* get_ys_ptr() const {
+        return ys->data();
+    }
+
+    float* get_zs_ptr() const {
+        return zs->data();
+    }
+
+    float* get_as_ptr() const {
+        as->data();
+    }
+
+private:
+    DeviceBufferRAII<float>::u_ptr xs;
+    DeviceBufferRAII<float>::u_ptr ys;
+    DeviceBufferRAII<float>::u_ptr zs;
+    DeviceBufferRAII<float>::u_ptr as;
+
+    size_t m_num_scatterers;
+};
+
+// Device memory for multiple fixed-scatterer datasets
+class DeviceFixedScatterersCollection {
+public:
+    // create a new dataset and fill it with data (will allocate memory on device)
+    void add(bcsim::FixedScatterers::s_ptr host_scatterers) {
+        auto new_device_scatterers = std::make_shared<DeviceFixedScatterers>(host_scatterers->num_scatterers());
+        transfer_to_device(host_scatterers, new_device_scatterers);
+        m_fixed_datasets.push_back(new_device_scatterers);
+    }
+
+    void transfer_to_device(bcsim::FixedScatterers::s_ptr host_scatterers,
+                            DeviceFixedScatterers::s_ptr device_scatterers) {
+        const auto num_scatterers = host_scatterers->num_scatterers();
+
+        // reorganize and transfer
+        size_t bytes_per_component = num_scatterers*sizeof(float);
+
+        // temporary host memory for scatterer points
+        HostPinnedBufferRAII<float> host_temp(bytes_per_component);
+
+        // transfer x values
+        for (size_t i = 0; i < num_scatterers; i++) {
+            host_temp.data()[i] = host_scatterers->scatterers[i].pos.x;
+        }
+        cudaErrorCheck( cudaMemcpy(device_scatterers->get_xs_ptr(), host_temp.data(),
+                                   bytes_per_component, cudaMemcpyHostToDevice) );
+
+        // transfer y values
+        for (size_t i = 0; i < num_scatterers; i++) {
+            host_temp.data()[i] = host_scatterers->scatterers[i].pos.y;
+        }
+        cudaErrorCheck( cudaMemcpy(device_scatterers->get_ys_ptr(), host_temp.data(),
+                                   bytes_per_component, cudaMemcpyHostToDevice) );
+
+        // z values
+        for (size_t i = 0; i < num_scatterers; i++) {
+            host_temp.data()[i] = host_scatterers->scatterers[i].pos.z;
+        }
+        cudaErrorCheck( cudaMemcpy(device_scatterers->get_zs_ptr(), host_temp.data(),
+                                   bytes_per_component, cudaMemcpyHostToDevice) );
+
+        // a values
+        for (size_t i = 0; i < num_scatterers; i++) {
+            host_temp.data()[i] = host_scatterers->scatterers[i].amplitude;
+        }
+        cudaErrorCheck( cudaMemcpy(device_scatterers->get_as_ptr(), host_temp.data(),
+                                   bytes_per_component, cudaMemcpyHostToDevice) );
+    }
+
+    // update an existing dataset (will only reallocate memory if the new size is
+    // different from the previous)
+    void update(bcsim::FixedScatterers::s_ptr scatterer, size_t dset_idx) {
+        if (dset_idx >= m_fixed_datasets.size()) throw std::runtime_error("Illegal dataset index");
+        throw std::runtime_error("TODO");
+        /*
+        // check capacity of existsing
+    // no point in reallocating if we already have allocated memory and the number of bytes
+    // is the same.
+    bool reallocate_device_mem = true;
+    if (m_device_point_xs && m_device_point_ys && m_device_point_zs && m_device_point_as) {
+        if (   (m_device_point_xs->get_num_bytes() == points_common_bytes)
+            && (m_device_point_ys->get_num_bytes() == points_common_bytes)
+            && (m_device_point_zs->get_num_bytes() == points_common_bytes)
+            && (m_device_point_as->get_num_bytes() == points_common_bytes))
+        {
+            reallocate_device_mem = false;
+        }
+        */
+        // if does not match, reallocate
+
+        // transfer from host to device.
+
+    }
+
+    size_t get_total_num_scatterers() const {
+        size_t sum = 0;
+        for (size_t i = 0; i < m_fixed_datasets.size(); i++) {
+            sum += m_fixed_datasets[i]->get_num_scatterers();
+        }
+        return sum;
+    }
+
+    size_t get_num_datasets() const {
+        return m_fixed_datasets.size();
+    }
+
+    DeviceFixedScatterers::s_ptr get_dataset(size_t dset_idx) const {
+        if (dset_idx >= m_fixed_datasets.size()) throw std::runtime_error("Illegal dataset index");
+        return m_fixed_datasets[dset_idx];
+    }
+
+private:
+    std::vector<DeviceFixedScatterers::s_ptr>   m_fixed_datasets;
+};
+
+
 class GpuAlgorithm : public BaseAlgorithm {
 public:
     GpuAlgorithm();
