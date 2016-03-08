6b54c210a23b00494609ed482ff9d457f746def1
Sigurd Storve <sigurd.storve@ntnu.no>
2016-03-08 15:16:31 +0100

    Made script more general.
    This is a preparation for a new script for demonstrating the use of
    multiple GPUs. The plan is to call do_simulation() from multiple threads
    with different devices.

--- python/linear_scan_phantom.py
+++ python/linear_scan_phantom.py
@@ -14,6 +14,83 @@ description="""
     runs.
 """
 
+def do_simulation(args):
+    if args.use_gpu:
+        sim = RfSimulator("gpu")
+        sim.set_parameter("gpu_device", "%d"%args.device_no)
+    else:
+        sim = RfSimulator("cpu")
+
+    sim.set_parameter("verbose", "0")
+
+    with h5py.File(args.h5_file, "r") as f:
+        scatterers_data = f["data"].value
+    sim.add_fixed_scatterers(scatterers_data)
+    print "The number of scatterers is %d" % scatterers_data.shape[0]
+
+    # configure simulation parameters
+    sim.set_parameter("sound_speed", "1540.0")
+    sim.set_parameter("radial_decimation", "10")
+    sim.set_parameter("phase_delay", "on")
+    sim.set_parameter("noise_amplitude", "%f" % args.noise_ampl)
+
+    # configure the RF excitation
+    fs = 80e6
+    ts = 1.0/fs
+    fc = 5.0e6
+    tc = 1.0/fc
+    t_vector = np.arange(-16*tc, 16*tc, ts)
+    bw = 0.3
+    samples = np.array(gausspulse(t_vector, bw=bw, fc=fc), dtype="float32")
+    center_index = int(len(t_vector)/2) 
+    sim.set_excitation(samples, center_index, fs, fc)
+
+    # define the scan sequence
+    origins = np.zeros((args.num_lines, 3), dtype="float32")
+    origins[:,0] = np.linspace(args.x0, args.x1, args.num_lines)
+    x_axis = np.array([1.0, 0.0, 0.0])
+    z_axis = np.array([0.0, 0.0, 1.0])
+    directions = np.array(np.tile(z_axis, (args.num_lines, 1)), dtype="float32")
+    length = 0.06
+    lateral_dirs = np.array(np.tile(x_axis, (args.num_lines, 1)), dtype="float32")
+    timestamps = np.zeros((args.num_lines,), dtype="float32")
+    sim.set_scan_sequence(origins, directions, length, lateral_dirs, timestamps)
+
+    # configure the beam profile
+    sim.set_analytical_beam_profile(1e-3, 1e-3)
+
+    frame_sim_times = []
+    for frame_no in range(args.num_frames):
+        start_time = time()
+        iq_lines = sim.simulate_lines()
+        frame_sim_times.append(time()-start_time)
+        
+    if args.save_simdata_file != "":
+        with h5py.File(args.save_simdata_file, "w") as f:
+            f["sim_data_real"] = np.array(np.real(iq_lines), dtype="float32")
+            f["sim_data_imag"] = np.array(np.imag(iq_lines), dtype="float32")
+        print "Simulation output written to %s" % args.save_simdata_file
+        
+    print "Simulation time: %f +- %f s  (N=%d)" % (np.mean(frame_sim_times), np.std(frame_sim_times), args.num_frames)    
+
+    if args.pdf_file != "" and not args.visualize:
+        import matplotlib as mpl
+        mpl.use("Agg")
+    if args.pdf_file != "" or args.visualize:
+        import matplotlib.pyplot as plt
+        num_samples, num_lines = iq_lines.shape
+        plt.figure(1, figsize=(18,9))
+        plt.subplot(1,2,1)
+        plt.plot(np.real(iq_lines[:, num_lines/2]))
+        plt.title("Middle RF line")
+        plt.subplot(1,2,2)
+        plt.imshow(np.real(abs(iq_lines)), aspect="auto", interpolation="nearest")
+        if args.pdf_file != "":
+            plt.savefig(args.pdf_file)
+            print "Image written to disk."
+    if args.visualize:
+        plt.show()
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description=description)
     parser.add_argument("h5_file", help="Hdf5 file with scatterers")
@@ -22,85 +99,11 @@ if __name__ == "__main__":
     parser.add_argument("--num_lines", type=int, default=192)
     parser.add_argument("--num_frames", help="Each frame is equal, but can be used to test performance", type=int, default=1)
     parser.add_argument("--visualize", help="Visualize the middle RF line", action="store_true")
-    parser.add_argument("--save_pdf", help="Save .pdf image", action="store_true")
+    parser.add_argument("--pdf_file", help="Name of pdf file to save, if not empty", default="")
     parser.add_argument("--device_no", help="GPU device no to use", type=int, default=0)
     parser.add_argument("--use_gpu", action="store_true")
-    parser.add_argument("--save_simdata_file", help="Export simulated data in a format that can be loaded in the GUI app.", type=str, default=None)
+    parser.add_argument("--save_simdata_file", help="Export simulated data in a format that can be loaded in the GUI app.", type=str, default="")
+    parser.add_argument("--noise_ampl", help="Simulator noise", type=float, default=0)
     args = parser.parse_args()
-
-if args.use_gpu:
-    sim = RfSimulator("gpu")
-    sim.set_parameter("gpu_device", "%d"%args.device_no)
-else:
-    sim = RfSimulator("cpu")
-
-
-sim.set_parameter("verbose", "0")
-
-with h5py.File(args.h5_file, "r") as f:
-    scatterers_data = f["data"].value
-sim.add_fixed_scatterers(scatterers_data)
-print "The number of scatterers is %d" % scatterers_data.shape[0]
-
-# configure simulation parameters
-sim.set_parameter("sound_speed", "1540.0")
-sim.set_parameter("radial_decimation", "30")
-
-# configure the RF excitation
-fs = 100e6
-ts = 1.0/fs
-fc = 2.5e6
-tc = 1.0/fc
-t_vector = np.arange(-16*tc, 16*tc, ts)
-bw = 0.2
-samples = np.array(gausspulse(t_vector, bw=bw, fc=fc), dtype="float32")
-center_index = int(len(t_vector)/2) 
-sim.set_excitation(samples, center_index, fs, fc)
-
-# define the scan sequence
-origins = np.zeros((args.num_lines, 3), dtype="float32")
-origins[:,0] = np.linspace(args.x0, args.x1, args.num_lines)
-x_axis = np.array([1.0, 0.0, 0.0])
-z_axis = np.array([0.0, 0.0, 1.0])
-directions = np.array(np.tile(z_axis, (args.num_lines, 1)), dtype="float32")
-length = 0.12
-lateral_dirs = np.array(np.tile(x_axis, (args.num_lines, 1)), dtype="float32")
-timestamps = np.zeros((args.num_lines,), dtype="float32")
-sim.set_scan_sequence(origins, directions, length, lateral_dirs, timestamps)
-
-# configure the beam profile
-sim.set_analytical_beam_profile(1e-3, 1e-3)
-
-frame_sim_times = []
-for frame_no in range(args.num_frames):
-    start_time = time()
-    rf_lines = sim.simulate_lines()
-    frame_sim_times.append(time()-start_time)
-
-# get envelope of IQ data
-rf_lines = np.real(abs(rf_lines))    
     
-if args.save_simdata_file != None:
-    with h5py.File(args.save_simdata_file, "w") as f:
-        f["sim_data"] = np.array(rf_lines, dtype="float32")
-    print "Simulation output written to %s" % args.save_simdata_file
-    
-print "Simulation time: %f +- %f s  (N=%d)" % (np.mean(frame_sim_times), np.std(frame_sim_times), args.num_frames)    
-
-if args.save_pdf or args.visualize:
-    import matplotlib as mpl
-    mpl.use("Agg")
-    import matplotlib.pyplot as plt
-    num_samples, num_lines = rf_lines.shape
-    plt.figure(1)
-    plt.plot(rf_lines[:, num_lines/2])
-    if args.save_pdf: plt.savefig("frame1-out.pdf")
-    plt.figure(2)
-    plt.imshow(rf_lines, aspect="auto")
-    if args.save_pdf: plt.savefig("frame2-out.pdf")
-if args.visualize:
-    plt.show()
-if args.save_pdf:
-    print "Image written to disk."
-    
-    
+    do_simulation(args)
