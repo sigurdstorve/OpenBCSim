abe482798e6ac18035d825616de0ae02e6f559fd
Sigurd Storve <sigurd.storve@ntnu.no>
2016-05-23 23:16:18 +0200

    Fixed error in beam profile simulation script

--- matlab/sim_lut_hdf5.m
+++ matlab/sim_lut_hdf5.m
@@ -12,30 +12,31 @@ function sim_lut_hdf5(h5_file, xmit_aperture, receive_aperture, geo)
     xs_ = linspace(geo.x_min, geo.x_max, geo.num_x);
     ys_ = linspace(geo.y_min, geo.y_max, geo.num_y);
     zs_ = linspace(geo.z_min, geo.z_max, geo.num_z);
-    [xx, yy, zz] = ndgrid(xs_, ys_, zs_); 
-    all_points = [xx(:) yy(:) zz(:)];
-
-    [p, start_time] = calc_hhp(xmit_aperture, receive_aperture, all_points);
-
-    % Map to scalar intensities
-    % dim1: time
-    % dim2: point index
-    % TODO: use sum of squares instead of just max value in time dimension?
-    intensities = max(p, [], 1);
+    
+    % nested loops instead of simulating all points in one batch due to
+    % observed out-of-memory issues observed with the batch-solution..
+    %
+    % dimensions are in reverse order since Matlab uses column-major and
+    % the HDF5 library uses row-major..
+    % (the simulator expects: dim0~radial, dim1~lateral, dim2~elevational)
+    intensities = zeros(geo.num_y, geo.num_x, geo.num_z);
+    for ix = 1:geo.num_x
+        fprintf('ix = %d of %d\n', ix, geo.num_x)
+        for iy = 1:geo.num_y
+            for iz = 1:geo.num_z
+                [p, start_time] = calc_hhp(xmit_aperture, receive_aperture, [xs_(ix) ys_(iy) zs_(iz)]);
+                % TODO: use sum of squares instead of just max value in time dimension?
+                intensities(iy, ix, iz) = max(p);
+            end
+        end
+    end
 
     % map intensities to [0, 1]
-    min_value = min(intensities);
-    max_value = max(intensities);
+    min_value = min(intensities(:));
+    max_value = max(intensities(:));
     intensities = (intensities - min_value)/(max_value - min_value);
 
-    % reshape
-    intensities = reshape(intensities, [geo.num_x geo.num_y geo.num_z]);
-
-    % reorder dimensions to expected: [z, x, y] ~ [rad, lat, ele]
-    % ==> not needed size matlab uses column-major...
-    %intensities = permute(intensities, [3, 2, 1]);
-
-    % store lookup-table and geometry to hdf5 file
+    % store lookup-table and geometry to hdf5 file [using 32-bit floats]
     h5create(h5_file, '/beam_profile', size(intensities), 'DataType', 'single');
     h5write(h5_file, '/beam_profile', intensities);
     h5create(h5_file, '/ele_extent', 2, 'DataType', 'single');
