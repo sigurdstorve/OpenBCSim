5e8c139936941be06344db36f1e45a618c570f90
Sigurd Storve <sigurd.storve@ntnu.no>
2016-05-31 21:49:34 +0200

    Added Matlab script for using Field II to simulate a phased array cardiac probe

---
+++ matlab/sim_profile_cardiac.m
@@ -0,0 +1,93 @@
+field_init(0);
+
+% Script for simulating two-way pulse-echo profile for
+% a phased array cardiac probe.
+% Based on the example http://field-ii.dk/?examples/kidney_example/kidney_example.html
+
+center_freq = 3.0e6;
+fs = 100e6;
+c0 = 1540.0;
+lambda = c0/center_freq;
+el_width = 0.5*lambda;
+el_height = 15e-3;
+kerf = lambda/20;
+num_elements = 71;
+
+probe_width = num_elements*(el_width+kerf);
+fprintf('Probe width is %2.1f mm\n', 1000*probe_width);
+
+% fixed transmission focus
+tx_focus = 70e-3;
+
+% duration of impulse response
+imp_resp_num_cycles = 2;
+
+% duration of excitation signal
+excitation_num_cycles = 2;
+
+% name of output file with lookup-table. WARNING: Will be overwritten!
+h5_file_out = 'beam_profile_cardiac.h5';
+
+set_sampling(fs);
+set_field('show_times', 5);
+
+% transmission aperture
+tx_aperture = xdc_linear_array(num_elements, el_width, el_height, kerf, 1, 5, [0.0 0.0 tx_focus]);
+
+% impulse response and excitation for the transmission aperture
+imp_resp_times = 0:(1.0/fs):(imp_resp_num_cycles/center_freq);
+imp_resp = sin(2*pi*center_freq*imp_resp_times);
+figure(1);
+plot(imp_resp_times, imp_resp);
+title('Impulse response');
+xlabel('Time [s]');
+
+imp_resp = imp_resp.*hanning(max(size(imp_resp)))';
+xdc_impulse(tx_aperture, imp_resp);
+
+excitation_times = 0:(1.0/fs):(excitation_num_cycles/center_freq);
+excitation = sin(2*pi*center_freq*excitation_times);
+figure(2);
+plot(excitation_times, excitation);
+title('Excitation signal');
+xlabel('Time [s]');
+
+xdc_excitation(tx_aperture, excitation);
+
+% reception aperture
+rx_aperture = xdc_linear_array(num_elements, el_width, el_height, kerf, 1, 5, [0.0 0.0 tx_focus]);
+xdc_impulse(rx_aperture, imp_resp);
+
+% configure different focal zones for dynamic focusing on reception
+focal_zones = [5e-3:1e-3:150e-3]';
+num_focal_zones = max(size(focal_zones));
+fprintf('Number of focal zones is %d\n', num_focal_zones);
+focus_times = (focal_zones - 10e-3) / c0;
+
+% set apodization
+apodization = hanning(num_elements)';
+xdc_apodization(tx_aperture, 0, apodization);
+xdc_apodization(rx_aperture, 0, apodization);
+
+% focus at this angle
+theta = 0.0;
+xdc_focus(tx_aperture, 0, [tx_focus*sin(theta) 0.0 tx_focus*cos(theta)]);
+xdc_focus(rx_aperture, focus_times, [focal_zones*sin(theta) zeros(max(size(focal_zones)), 1) focal_zones*cos(theta)]);
+
+% create geometry struct for volume in which to simulate two-way sensitivity
+geo = struct();
+geo.z_min = 1e-3;
+geo.z_max = 160e-3;
+geo.num_z = 1024;
+geo.x_min = -1.2e-2;
+geo.x_max = 1.2e-2;
+geo.num_x = 128;
+geo.y_min = -1.2e-2;
+geo.y_max = 1.2e-2;
+geo.num_y = 128;
+
+delete(h5_file_out);
+sim_lut_hdf5(h5_file_out, tx_aperture, rx_aperture, geo);
+
+xdc_free(tx_aperture);
+xdc_free(rx_aperture);
