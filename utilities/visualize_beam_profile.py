87dbb3772470e6b477b1591ac281ab47f1a3bb57
Sigurd Storve <sigurd.storve@ntnu.no>
2016-06-07 07:42:50 +0200

    improved beam profile visualization script

--- utilities/visualize_beam_profile.py
+++ utilities/visualize_beam_profile.py
@@ -11,6 +11,8 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description=description)
     parser.add_argument("h5_file")
     parser.add_argument("--normalize", action="store_true", help="normalize in [0, 1]")
+    parser.add_argument("--interp", default="nearest", help="Matplotlib interpolation type")
+    parser.add_argument("--dyn_range", help="Dynamic range for plotting", type=int, default=70)
     args = parser.parse_args()
     
     with h5py.File(args.h5_file, "r") as f:
@@ -19,9 +21,9 @@ if __name__ == "__main__":
         lat_extent = f["lat_extent"].value
         rad_extent = f["rad_extent"].value
     
-    print "Radial extent: %s" % str(rad_extent)
-    print "Lateral extent: %s" % str(lat_extent)
-    print "Elevational extent: %s" % str(ele_extent)        
+    print "Radial extent: %s m" % str(rad_extent)
+    print "Lateral extent: %s m" % str(lat_extent)
+    print "Elevational extent: %s m" % str(ele_extent)        
     
     min_value = np.min(beam_profile.flatten())
     max_value = np.max(beam_profile.flatten())
@@ -45,8 +47,11 @@ if __name__ == "__main__":
     # radial-lateral [assumes symmetric lateral/elevational extents]
     ele_dim = num_ele_samples/2
     rad_lat_slice = beam_profile[:, :, ele_dim]
-    plt.imshow(rad_lat_slice.transpose(), extent=[rad_extent[0], rad_extent[1], lat_extent[0], lat_extent[1]], interpolation="nearest")
+    plt.imshow(20*np.log10(rad_lat_slice.transpose()), extent=[rad_extent[0]*1000, rad_extent[1]*1000, lat_extent[0]*1000, lat_extent[1]*1000], interpolation=args.interp)
     plt.title("Radial-Lateral")
+    plt.xlabel("millimeters")
+    plt.ylabel("millimeters")
+    plt.clim(0.0, -args.dyn_range)
     plt.colorbar()
     plt.gca().set_aspect("equal")
     
@@ -54,21 +59,26 @@ if __name__ == "__main__":
     plt.figure()
     lat_dim = num_lat_samples/2
     rad_ele_slice = beam_profile[:, lat_dim, :]
-    plt.imshow(rad_ele_slice.transpose(), extent=[rad_extent[0], rad_extent[1], ele_extent[0], ele_extent[1]], interpolation="nearest")
+    plt.imshow(20*np.log10(rad_ele_slice.transpose()), extent=[rad_extent[0]*1000, rad_extent[1]*1000, ele_extent[0]*1000, ele_extent[1]*1000], interpolation=args.interp)
     plt.title("Radial-Elevational")
+    plt.xlabel("millimeters")
+    plt.ylabel("millimeters")
+    plt.clim(0.0, -args.dyn_range)
     plt.colorbar()
     plt.gca().set_aspect("equal")
 
     plt.figure()
     # short-axis plots for all radial distances
-    img_extent = [ele_extent[0], ele_extent[1], lat_extent[0], lat_extent[1]] # riktig?
+    img_extent = [ele_extent[0]*1000, ele_extent[1]*1000, lat_extent[0]*1000, lat_extent[1]*1000]
     for r_idx in range(num_rad_samples):
         plt.clf()
-        plt.imshow(beam_profile[r_idx,:,:], extent=img_extent, cmap="Greys_r", aspect="auto", interpolation="nearest")
+        plt.imshow(beam_profile[r_idx,:,:], extent=img_extent, cmap="Greys_r", aspect="auto", interpolation=args.interp)
         plt.draw()
         plt.title("Lat-Ele: Radial index %d of %d" % (r_idx+1, num_rad_samples))
         if args.normalize:
             plt.clim(0.0, 1.0)
+        plt.xlabel("millimeters")
+        plt.ylabel("millimeters")
         plt.colorbar()
+        plt.gca().set_aspect("equal")
         raw_input("Press enter")
-    
