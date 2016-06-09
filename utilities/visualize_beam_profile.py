afdccc20817a50c80d47b714f9bf49dc16ae90cd
Sigurd Storve <sigurd.storve@ntnu.no>
2016-06-09 23:03:38 +0200

    Publication-quality long-axis beam profiles

--- utilities/visualize_beam_profile.py
+++ utilities/visualize_beam_profile.py
@@ -20,6 +20,7 @@ if __name__ == "__main__":
     parser.add_argument("--cmap", help="Specify custom colormap name", default="Greys_r")
     parser.add_argument("--db_scale", help="Plot in decibels", action="store_true")
     parser.add_argument("--save_pdf", help="Save PDF figures", action="store_true")
+    parser.add_argument("--pdf_dpi", help="DPI for image in PDF", type=float, default=300.0)
     args = parser.parse_args()
     
     with h5py.File(args.h5_file, "r") as f:
@@ -52,57 +53,68 @@ if __name__ == "__main__":
     
     if args.normalize:
         beam_profile = (beam_profile-min_value)/(max_value-min_value)
-        
-    plt.figure()
+    
+    
+    fig, (ax0, ax1) = plt.subplots(nrows=2, ncols=1, sharex=True)
     plt.ion()
     plt.show()
     
-    db_ticks = np.linspace(0.0, -args.dyn_range, 5)
     if args.db_scale:
+        vmin = -args.dyn_range
+        vmax = 0.0
+        cb_ticks = np.linspace(0.0, -args.dyn_range, 5)
         trans_function = lambda x: 20.0*np.log10(x)
     else:
+        vmin = 0.0
+        vmax = 1.0
+        cb_ticks = np.linspace(0.0, 1.0, 5)
         trans_function = lambda x: x
         
     # radial-lateral [assumes symmetric lateral/elevational extents]
     ele_dim = num_ele_samples/2
     rad_lat_slice = beam_profile[:, :, ele_dim]
-    plt.imshow(trans_function(rad_lat_slice.transpose()), extent=[rad_extent[0]*1000, rad_extent[1]*1000, lat_extent[0]*1000, lat_extent[1]*1000], interpolation=args.interp, cmap=args.cmap)
-    plt.title("Radial-Lateral")
-    plt.xlabel("millimeters")
-    plt.ylabel("millimeters")
-    cb = plt.colorbar(format=FormatStrFormatter("%2.0f"))
-    if args.db_scale:
-        plt.clim(0.0, -args.dyn_range)
-        cb.set_label("dB")
-        cb.set_ticks(db_ticks)
-    else:
-        plt.clim(0.0, 1.0)
-    plt.gca().set_aspect("equal")
-    if args.rad_extent != None: plt.xlim(*args.rad_extent)
-    if args.lat_extent != None: plt.ylim(*args.lat_extent)
-    if args.save_pdf:
-        plt.savefig("beam_profile_rad_lat.pdf", bbox_inches="tight", pad_inches=0.1, dpi=300)
+    im0 = ax0.imshow(trans_function(rad_lat_slice.transpose()), extent=[rad_extent[0]*1000, rad_extent[1]*1000, lat_extent[0]*1000, lat_extent[1]*1000],
+                     interpolation=args.interp, cmap=args.cmap, vmin=vmin, vmax=vmax)
+    ax0.set_ylabel("Lateral [mm]")
+    ax0.set_aspect("equal")
+    ax0.spines['top'].set_visible(False)
+    ax0.spines['right'].set_visible(False)
+    ax0.spines['bottom'].set_visible(False)
+    ax0.spines['left'].set_visible(False)
+    ax0.yaxis.set_ticks_position('left')
+    ax0.xaxis.set_ticks_position('bottom')
+    if args.rad_extent != None: ax0.set_xlim(*args.rad_extent)
+    if args.lat_extent != None: ax0.set_ylim(*args.lat_extent)
     
     # radial-elevational [assumes symmetric lateral/elevational extents]
-    plt.figure()
     lat_dim = num_lat_samples/2
     rad_ele_slice = beam_profile[:, lat_dim, :]
-    plt.imshow(trans_function(rad_ele_slice.transpose()), extent=[rad_extent[0]*1000, rad_extent[1]*1000, ele_extent[0]*1000, ele_extent[1]*1000], interpolation=args.interp, cmap=args.cmap)
-    plt.title("Radial-Elevational")
-    plt.xlabel("millimeters")
-    plt.ylabel("millimeters")
-    cb = plt.colorbar(format=FormatStrFormatter("%2.0f"))
-    if args.db_scale:
-        plt.clim(0.0, -args.dyn_range)
-        cb.set_label("dB")
-        cb.set_ticks(db_ticks)
-    else:
-        plt.clim(0.0, 1.0)
-    plt.gca().set_aspect("equal")
+    im1 = ax1.imshow(trans_function(rad_ele_slice.transpose()), extent=[rad_extent[0]*1000, rad_extent[1]*1000, ele_extent[0]*1000, ele_extent[1]*1000],
+                     interpolation=args.interp, cmap=args.cmap, vmin=vmin, vmax=vmax)
+    ax1.set_ylabel("Elevational [mm]")
+    ax1.set_aspect("equal")
+    ax1.spines['top'].set_visible(False)
+    ax1.spines['right'].set_visible(False)
+    ax1.spines['bottom'].set_visible(False)
+    ax1.spines['left'].set_visible(False)
+    ax1.yaxis.set_ticks_position('left')
+    ax1.xaxis.set_ticks_position('bottom')
     if args.rad_extent != None: plt.xlim(*args.rad_extent)
     if args.ele_extent != None: plt.ylim(*args.ele_extent)
+
+    plt.suptitle("Pulse-echo sensitivity")
+    ax1.set_xlabel("Radial distance [mm]")
+    fig.subplots_adjust(right=0.8)
+    cbar_ax = fig.add_axes([0.85, 0.15, 0.05, 0.7])
+    cb = fig.colorbar(im1, format=FormatStrFormatter("%2.0f"), cax=cbar_ax, ticks=cb_ticks)
+    cb.set_clim(-args.dyn_range, 0.0)    
+    cb.draw_all() # needed for some reason
+    if args.db_scale:
+        cb.set_label("dB")
+    cb.outline.set_visible(False)
+
     if args.save_pdf:
-        plt.savefig("beam_profile_rad_ele.pdf", bbox_inches="tight", pad_inches=0.1, dpi=300)
+        plt.savefig("beam_profile_pulse_echo.pdf", bbox_inches="tight", pad_inches=0.1, dpi=args.pdf_dpi)
     
     plt.figure()
     # short-axis plots for all radial distances
