f356c573c410233d39c3986ae09a64607030feb9
Sigurd Storve <sigurd.storve@ntnu.no>
2016-07-03 14:24:20 +0200

    Updated widget to load shaders from resource files

--- src/qt5gui/GLScattererWidget.cpp
+++ src/qt5gui/GLScattererWidget.cpp
@@ -115,36 +115,6 @@ void GLScattererWidget::cleanup() {
     doneCurrent();
 }
 
-static const char* vertexShaderSourceCore =
-    "#version 150\n"
-    "in vec4 vertex;\n"
-    "in vec3 normal;\n"
-    "out vec3 vert;\n"
-    "out vec3 vertNormal;\n"
-    "uniform mat4 projMatrix;\n"
-    "uniform mat4 mvMatrix;\n"
-    "uniform mat3 normalMatrix;\n"
-    "void main() {\n"
-    "   vert = vertex.xyz;\n"
-    "   vertNormal = normalMatrix*normal;\n"
-    "   gl_Position = projMatrix*mvMatrix*vertex;\n"
-    "   gl_PointSize = 10;"
-    "}\n";
-
-static const char* fragmentShaderSourceCore =
-    "#version 150\n"
-    "in highp vec3 vert;\n"
-    "in highp vec3 vertNormal;\n"
-    "out highp vec4 fragColor;\n"
-    "uniform highp vec3 lightPos;\n"
-    "void main() {\n"
-    "   highp vec3 L = normalize(lightPos - vert);\n"
-    "   highp float NL = max(dot(normalize(vertNormal), L), 0.0);\n"
-    "   highp vec3 color = vec3(0.0, 1.0, 0.0);\n"
-    "   highp vec3 col = clamp(color*0.2+color*0.8*NL, 0.0, 1.0);\n"
-    "   fragColor = vec4(col, 1.0);\n"
-    "}\n";
-
 void GLScattererWidget::initializeGL() {
     // Handle reinitialize in case top-level widget changes
     connect(context(), &QOpenGLContext::aboutToBeDestroyed, this, &GLScattererWidget::cleanup);
@@ -155,11 +125,11 @@ void GLScattererWidget::initializeGL() {
 
     glClearColor(0, 0, 0, m_transparent ? 0 : 1);
     
-    m_program = new QOpenGLShaderProgram;
-    if (!m_program->addShaderFromSourceCode(QOpenGLShader::Vertex, vertexShaderSourceCore)) {
+    m_program = new QOpenGLShaderProgram; // TODO: Use smart-pointer?
+    if (!m_program->addShaderFromSourceFile(QOpenGLShader::Vertex, ":/scatterer_vshader.glsl")) {
         throw std::runtime_error("Failed to compile vertex shader");
     }
-    if (!m_program->addShaderFromSourceCode(QOpenGLShader::Fragment, fragmentShaderSourceCore)) {
+    if (!m_program->addShaderFromSourceFile(QOpenGLShader::Fragment, ":/scatterer_fshader.glsl")) {
         throw std::runtime_error("Failed to compile fragment shader");
     }
     m_program->bindAttributeLocation("vertex", 0);
