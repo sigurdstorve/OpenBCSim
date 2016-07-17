ea0411e5229b3170885b5c6f708166cefa19026d
Sigurd Storve <sigurd.storve@ntnu.no>
2016-07-17 10:42:18 +0200

    Added CSV reader

---
+++ src/utils/CSVReader.cpp
@@ -0,0 +1,60 @@
+#include <fstream>
+#include <stdexcept>
+#include "CSVReader.hpp"
+
+namespace csv {
+
+CSVReader::CSVReader(std::istream& instream, char delimiter, LogCallback log_callback)
+    : m_log_callback([](const std::string&) {}),
+      m_delimiter(delimiter)
+{
+    if (log_callback) m_log_callback = log_callback;
+    read_column_headers(instream);
+    read_and_store_columns_as_string(instream);
+}
+CSVReader::CSVReader(const std::string& filename, char delimiter, LogCallback log_callback)
+    : CSVReader(std::ifstream(filename, std::ios::in), delimiter, log_callback) { }
+
+void CSVReader::read_column_headers(std::istream& instream) {
+    std::string str;
+    if (std::getline(instream, str)) {
+        m_column_headers = split_string(str);
+    } else {
+        throw std::runtime_error("unable to read column headers");
+    }
+    m_log_callback("csv file has " + std::to_string(m_column_headers.size()) + " columns");
+}
+
+std::vector<std::string> CSVReader::split_string(const std::string& s) {
+    std::vector<std::string> res;
+    std::stringstream ss(s);
+    std::string item;
+    while (std::getline(ss, item, m_delimiter)) {
+        res.push_back(item);
+    }
+    return res;
+}
+
+void CSVReader::read_and_store_columns_as_string(std::istream& instream) {
+    std::string cur_line;
+    const auto num_columns = m_column_headers.size();
+    while (std::getline(instream, cur_line)) {
+        m_log_callback("Parsing line: " + cur_line);
+        auto string_parts = split_string(cur_line);
+        if (string_parts.size() != num_columns) {
+            throw std::runtime_error("mismatch between number of row entries and number of column headers");
+        }
+        for (size_t i = 0; i < num_columns; i++) {
+            m_string_data[m_column_headers[i]].push_back(string_parts[i]);
+        }
+    }
+}
+
+const std::vector<std::string>& CSVReader::get_column_strings_or_throw(const std::string& col_name) {
+    if (m_string_data.find(col_name) == std::end(m_string_data)) {
+        throw std::runtime_error("no column found");
+    }
+    return m_string_data[col_name];
+}
+
+}   // end namespace
---
+++ src/utils/CSVReader.hpp
@@ -0,0 +1,49 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <functional>
+#include <vector>
+#include <map>
+#include <algorithm>
+#include <sstream>
+
+namespace csv {
+    
+class CSVReader {
+public:
+    typedef std::function<void(const std::string&)> LogCallback;
+    
+    CSVReader(const std::string& filename, char delimiter=';', LogCallback log_callback=nullptr);
+
+    CSVReader(std::istream& instream, char delimiter=';', LogCallback log_callback=nullptr);
+
+    template <typename T>
+    std::vector<T> get_column(const std::string& col_name) {
+        std::vector<T> res;
+        auto& strings = get_column_strings_or_throw(col_name);
+        std::transform(std::begin(strings), std::end(strings), std::back_inserter(res), [](const std::string& s) {
+            T temp;
+            std::stringstream ss(s);
+            ss >> temp;
+            return temp;
+        });
+        return res;
+    }
+
+private:
+    void read_column_headers(std::istream& instream);
+
+    void read_and_store_columns_as_string(std::istream& instream);
+
+    const std::vector<std::string>& get_column_strings_or_throw(const std::string& col_name);
+
+    std::vector<std::string> split_string(const std::string& s);
+
+private:
+    LogCallback                                         m_log_callback;
+    std::map<std::string, std::vector<std::string>>     m_string_data;
+    char                                                m_delimiter;
+    std::vector<std::string>                            m_column_headers;
+};
+
+}
