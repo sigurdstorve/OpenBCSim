b5a943ddc589da7922bb6c6729d4d294845ad6e9
Sigurd Storve <sigurd.storve@ntnu.no>
2016-07-17 22:29:32 +0200

    Finished first version of the LV phantom factory

--- src/utils/DefaultPhantoms.hpp
+++ src/utils/DefaultPhantoms.hpp
@@ -1,5 +1,9 @@
 #pragma once
+#include <iostream>
 #include <memory>
+#include <random>
+#include <vector>
+#include "EllipsoidGeometry.hpp"
 #include "core/BCSimConfig.hpp"
 
 namespace default_phantoms {
@@ -34,6 +38,39 @@ struct LeftVentriclePhantomParameters {
 // Create a LV spline phantom model by randomly distributing point-scatterers in a box and then removing
 // those who are not inside a 3D capped ellipsoid. The remaining points are then scaled and rotated
 // to generate control points for splines.
-bcsim::Scatterers::u_ptr CreateLeftVentricle3dPhantom(const LeftVentriclePhantomParameters& type);
+class LeftVentricle3dPhantomFactory {
+public:
+    typedef std::function<void(const std::string&)> LogCallback;
+
+    // csv_stream is any stream which contains csv data [for the scaling signal]
+    LeftVentricle3dPhantomFactory(const LeftVentriclePhantomParameters& params, std::istream& csv_stream, LogCallback = nullptr);
+
+    bcsim::SplineScatterers::u_ptr get();
+
+private:
+    // creates an interpolated function from samples loaded from CSV
+    void load_csv_scale_signal(std::istream&);
+
+    // fill a rectangular region of space with uniformly random scatterers
+    void create_random_scatterers_in_box(size_t num_scatterers, float thickness);
+
+    // create uniformly random amplitudes
+    void create_random_amplitudes(size_t num, float low, float high);
+
+    // use the mathematical shape model to filter out unwanted scatterers
+    void remove_scatterers_outside();
+
+    // create the scatterer splines
+    void create_splines(const LeftVentriclePhantomParameters& params);
+
+private:
+    ellipsoid::Region3D                 m_box_region;
+    ellipsoid::ThickCappedZEllipsoid    m_mathematical_model;
+    std::vector<ellipsoid::Point3D>     m_points;
+    std::vector<float>                  m_amplitudes;
+    std::function<float(float)>         m_scale_function;
+    LogCallback                         m_log_callback;
+    bcsim::SplineScatterers::u_ptr      m_spline_scatterers;
+};
 
 }   // end namespace
